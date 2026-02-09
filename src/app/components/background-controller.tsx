57bb88df93691a5d218d9fc5f9554ffe42fa344d
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2026-02-09 15:42:32 +0000

    I need more gradient background, it not prominent

--- src/app/components/background-controller.tsx
+++ src/app/components/background-controller.tsx
@@ -6,15 +6,15 @@ export function BackgroundController() {
       <div className="relative h-full w-full bg-background">
         <div className="absolute h-full w-full mix-blend-multiply dark:mix-blend-color-dodge">
           <div
-            className="animate-blob absolute -top-1/4 -left-1/4 h-1/2 w-1/2 rounded-full bg-emerald-300 opacity-50 filter blur-3xl"
+            className="animate-blob absolute -top-1/4 -left-1/4 h-2/3 w-2/3 rounded-full bg-emerald-300 opacity-60 filter blur-3xl"
             style={{ animationDuration: '30s' }}
           ></div>
           <div
-            className="animate-blob absolute -bottom-1/4 -right-1/4 h-1/2 w-1/2 rounded-full bg-sky-300 opacity-50 filter blur-3xl"
+            className="animate-blob absolute -bottom-1/4 -right-1/4 h-2/3 w-2/3 rounded-full bg-sky-300 opacity-60 filter blur-3xl"
             style={{ animationDelay: '5s', animationDuration: '35s' }}
           ></div>
           <div
-            className="animate-blob absolute -bottom-1/4 left-1/4 h-1/3 w-1/3 rounded-full bg-slate-300 opacity-50 filter blur-3xl"
+            className="animate-blob absolute -bottom-1/4 left-1/4 h-1/2 w-1/2 rounded-full bg-slate-300 opacity-60 filter blur-3xl"
             style={{ animationDelay: '10s', animationDuration: '40s' }}
           ></div>
         </div>
