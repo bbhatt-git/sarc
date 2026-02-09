5d07167f604233d2e2ad2fe195b9c918c7c12f76
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2026-02-09 16:17:35 +0000

    The bottom part of the hero section background section should fade a lit

--- src/app/home-view.tsx
+++ src/app/home-view.tsx
@@ -125,6 +125,7 @@ export default function HomeView() {
             </div>
           </motion.div>
         </div>
+        <div className="absolute bottom-0 left-0 right-0 h-48 bg-gradient-to-t from-background to-transparent z-10" />
       </section>
 
       {/* Stats Section */}
