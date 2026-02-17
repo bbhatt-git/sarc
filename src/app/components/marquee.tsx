346bac5c6af31b54fc165104ede930f9e69ce998
Bhupesh Bhatt <bhattbhupesh.369@gmail.com>
2026-02-17 16:43:02 +0000

    The gapping issue is only for te Vyan Madai and Bhupesh hatt, and Akit a

--- src/app/components/marquee.tsx
+++ src/app/components/marquee.tsx
@@ -6,6 +6,7 @@ interface MarqueeProps extends React.HTMLAttributes<HTMLDivElement> {
   reverse?: boolean;
   children: React.ReactNode;
   vertical?: boolean;
+  pauseOnHover?: boolean;
 }
 
 export function Marquee({
@@ -13,31 +14,35 @@ export function Marquee({
   reverse,
   children,
   vertical = false,
+  pauseOnHover = false,
   ...props
 }: MarqueeProps) {
   return (
     <div
       {...props}
       className={cn(
-        'group flex w-full overflow-hidden p-0 [--gap:1rem]',
+        'group flex overflow-hidden p-0 [--gap:1rem]',
         vertical ? 'flex-col' : 'flex-row',
+        '[gap:var(--gap)]',
         className
       )}
     >
       <div
-        className={cn('flex shrink-0 items-center justify-around [gap:var(--gap)]', {
+        className={cn('flex shrink-0 items-center justify-start [gap:var(--gap)]', {
           'animate-marquee': !vertical,
           'animate-marquee-vertical': vertical,
           '[animation-direction:reverse]': reverse,
+          'group-hover:[animation-play-state:paused]': pauseOnHover,
         })}
       >
         {children}
       </div>
       <div
-        className={cn('flex shrink-0 items-center justify-around [gap:var(--gap)]', {
+        className={cn('flex shrink-0 items-center justify-start [gap:var(--gap)]', {
           'animate-marquee': !vertical,
           'animate-marquee-vertical': vertical,
           '[animation-direction:reverse]': reverse,
+          'group-hover:[animation-play-state:paused]': pauseOnHover,
         })}
         aria-hidden="true"
       >
